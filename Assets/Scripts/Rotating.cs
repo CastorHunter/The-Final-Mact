27c8e03c433e126e4fe60fa7990c5c6772af1a0d
Clergue Maël <mael.clergue@gmail.com>
2026-01-31 00:17:57 +0100

    Checkpoints, Killing

--- Assets/Scripts/Rotating.cs
+++ Assets/Scripts/Rotating.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Rotating : MonoBehaviour
 {
+    public int speedx;
+    public int speedy;
+    public int speedz;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,6 @@ public class Rotating : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(speedx, speedy, speedz));
     }
 }
